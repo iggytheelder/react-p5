cfcd283576643dcfe8d068c146525e0d1d961e65
iggytheelder <ignacec@goldmail.etsu.edu>
2023-06-27 20:59:25 -0400

    added the processing sketch

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,33 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+import React from "react";
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+import dynamic from 'next/dynamic'
 
-      <Footer />
-    </div>
-  )
-}
+// Will only import `react-p5` on client-side
+const Sketch = dynamic(() => import('react-p5').then((mod) => mod.default), {
+  ssr: false,
+})
+
+let x = 50;
+let y = 50;
+
+export default (props) => {
+  const setup = (p5, canvasParentRef) => {
+    // use parent to render the canvas in this ref
+    // (without that p5 will render the canvas outside of your component)
+    p5.createCanvas(500, 500).parent(canvasParentRef);
+  };
+
+  const draw = (p5) => {
+    p5.background(0);
+    p5.ellipse(x, y, 70, 70);
+    // NOTE: Do not use setState in the draw function or in functions that are executed
+    // in the draw function...
+    // please use normal variables or class properties for these purposes
+    x++;
+  };
+
+  return <Sketch setup={setup} draw={draw} />;
+};
